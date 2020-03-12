b0421848eda674fae69bb77c0e5d62c3f01ca081
Sameer Ali <sam.eer.ali@hotmail.co.uk>
2020-03-12 21:34:50 +0000

    feat(account-filter): Adds filters to accounts panel

--- web/src/app/components/account/accounts-panel/accounts-panel.component.ts
+++ web/src/app/components/account/accounts-panel/accounts-panel.component.ts
@@ -1,6 +1,16 @@
-import { AccountService } from './../../../services/account/account.service';
-import { CreateAccountRequest, AccountType, Account } from './../../../models/account';
-import { Component, OnInit } from '@angular/core';
+import {
+  AccountService
+} from './../../../services/account/account.service';
+import {
+  CreateAccountRequest,
+  AccountType,
+  Account
+} from './../../../models/account';
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-accounts-panel',
@@ -12,29 +22,32 @@ export class AccountsPanelComponent implements OnInit {
   public account: CreateAccountRequest;
   public accountType = AccountType;
   public accounts: Account[];
-  constructor(private accountService: AccountService) {
-    this.account = {
-      name: '',
-      type: AccountType.Current,
-      initialAmount: 0
-    };
+  public displayAccounts: Account[];
+  public selectedAccount: SelectableAccounts;
+  public selectableAccounts = SelectableAccounts;
+
+  constructor(private accountService: AccountService, private cd: ChangeDetectorRef) {
+    this.clearForm();
     this.accounts = [];
     this.getAccounts();
+    this.selectedAccount = SelectableAccounts.All;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   createAccount() {
-    this.accountService.createAccount(this.account).subscribe((data: Account)  => {
-      this.accounts.push(data);
+    this.accountService.createAccount(this.account).subscribe((data: Account) => {
+      this.getAccounts();
       this.closeModal();
+      this.clearForm();
     });
   }
 
   getAccounts() {
     this.accountService.getAccounts().subscribe((data: Account[]) => {
       this.accounts = data;
+      this.updateDisplayAccounts();
+      this.cd.detectChanges();
     });
   }
 
@@ -46,4 +59,51 @@ export class AccountsPanelComponent implements OnInit {
     this.createAccountModalOpen = true;
   }
 
+  changeSelectedAccountType(accountType: SelectableAccounts) {
+    this.selectedAccount = accountType;
+    this.updateDisplayAccounts();
+  }
+
+  updateDisplayAccounts() {
+    switch (this.selectedAccount) {
+      case SelectableAccounts.All: {
+        this.displayAccounts = this.accounts;
+        break;
+      }
+      case SelectableAccounts.Current: {
+        this.displayAccounts = this.accounts.filter(a => a.type === AccountType.Current);
+        break;
+      }
+      case SelectableAccounts.Savings: {
+        this.displayAccounts = this.accounts.filter(a => a.type === AccountType.Savings);
+        break;
+      }
+      case SelectableAccounts.CreditCard: {
+        this.displayAccounts = this.accounts.filter(a => a.type === AccountType.CreditCard);
+        break;
+      }
+      case SelectableAccounts.Business: {
+        this.displayAccounts = this.accounts.filter(a => a.type === AccountType.Business);
+        break;
+      }
+    }
+
+  }
+
+  clearForm() {
+    this.account = {
+      name: '',
+      type: AccountType.Current,
+      initialAmount: 0
+    };
+  }
+}
+
+
+export enum SelectableAccounts {
+  All = 0,
+    Current = 1,
+    Business = 2,
+    Savings = 3,
+    CreditCard = 4
 }
