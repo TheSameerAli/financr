0a15fc524987c96e6a29c3d2741b1df13f5a34c4
Sameer Ali <sam.eer.ali@hotmail.co.uk>
2020-03-24 23:01:05 +0000

    feat(profit-loss): Adds profit/loss card on the dashboard

--- web/src/app/services/dashboard/dashboard.service.ts
+++ web/src/app/services/dashboard/dashboard.service.ts
@@ -8,6 +8,11 @@ export class DashboardService {
 
   constructor(private http: HttpClient) { }
 
+  getDashboardData() {
+    const uri = `/dashboard/dashboard-data`;
+    return this.http.get(uri);
+  }
+
   getNetworthByMonth() {
     const uri = `/dashboard/dashboard-chart`;
     return this.http.get(uri);
