ff101056aa72baf5fc6c506f36087703a89e4bfc
Sameer Ali <sam.eer.ali@hotmail.co.uk>
2020-03-29 16:15:40 +0100

    fix(login-bug): Fixes unable to change page after login

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,28 +1,42 @@
 import { UserService } from './services/user/user.service';
 import { User } from './models/user';
 import { AuthService } from './services/user/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   public isLoggedIn = false;
   public user: User;
   constructor(private authService: AuthService, private userService: UserService) {
-    if (authService.isLoggedIn()) {
+  }
+
+  ngOnInit(): void {
+    if (this.authService.isLoggedIn()) {
       this.isLoggedIn = true;
-      this.user = userService.getCurrentUser();
+      this.user = this.userService.getCurrentUser();
+      console.log(this.isLoggedIn);
     }
+
     this.authService.userLogoutEvent.subscribe((data) => {
       this.isLoggedIn = false;
+      this.user = {
+        id: '',
+        email: '',
+        password: '',
+        token: '',
+      };
+      console.log(this.isLoggedIn);
     });
+
     this.authService.userLoginEvent.subscribe(data => {
       this.isLoggedIn = true;
-      this.user = userService.getCurrentUser();
+      this.user = this.userService.getCurrentUser();
+      console.log(this.isLoggedIn);
     });
   }
-  title = 'frontend';
 }
