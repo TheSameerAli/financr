8f8aea72e8a8d4110dae56a765de75e6819c663d
Sameer Ali <sam.eer.ali@hotmail.co.uk>
2020-03-29 00:13:26 +0000

    feat(monthly-transactions): Adds monthly transaction panel to main dashboard

--- api/WebApi/Services/RecurringTransactionsService.cs
+++ api/WebApi/Services/RecurringTransactionsService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebApi.Context;
 using WebApi.Models.Database;
+using WebApi.Models.Database.Account;
 
 namespace WebApi.Services
 {
@@ -15,17 +16,19 @@ namespace WebApi.Services
         Task<List<RecurringTransaction>> GetAll(Guid accountId);
         Task<RecurringTransaction> Create(DateTimeOffset startDate, int occurrence, 
             double income, string description, Guid accountCategoryId, Guid accountId);
-
+        Task<List<RecurringTransaction>> GetAllSubscriptions(Guid userId);
         Task Delete(Guid recurringTransactionId);
     }
     public class RecurringTransactionsService : IRecurringTransactionsService
     {
         private readonly IUnitOfWork _uow;
         private readonly DbSet<RecurringTransaction> _recurringTransactions;
+        private readonly DbSet<Account> _accounts;
         public RecurringTransactionsService(IUnitOfWork uow)
         {
             _uow = uow;
             _recurringTransactions = _uow.Set<RecurringTransaction>();
+            _accounts = _uow.Set<Account>();
         }
         public async Task<List<RecurringTransaction>> GetAll()
         {
@@ -59,6 +62,18 @@ namespace WebApi.Services
                 await _recurringTransactions.FirstOrDefaultAsync(rt => rt.Id == recurringTransactionId);
             _recurringTransactions.Remove(recurringTransaction);
             await _uow.SaveChangesAsync();
+        }
+
+        public async Task<List<RecurringTransaction>> GetAllSubscriptions(Guid userId)
+        {
+            var recurringTransactions = new List<RecurringTransaction>();
+            var accounts = await _accounts.Where(a => a.UserId == userId).ToListAsync();
+            foreach (var account in accounts) 
+            {
+                recurringTransactions.AddRange(await _recurringTransactions.Where(rt => rt.AccountId == account.Id && rt.Income < 0)
+                .ToListAsync());
+            }
+            return recurringTransactions;
         }
     }
 }
