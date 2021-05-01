e979c2155c954b331cb7e28525ae1231a6f1bc43
Sameer Ali <sam.eer.ali@hotmail.co.uk>
2021-05-01 23:52:56 +0100

    feat(login-page-design): Implements login page design

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginPageComponent } from './pages/auth/login-page/login-page.component';
+import { FeatherModule } from 'angular-feather';
+import { ArrowRightCircle } from 'angular-feather/icons';
+const icons = {
+  ArrowRightCircle,
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FeatherModule.pick(icons)
   ],
   providers: [],
   bootstrap: [AppComponent]
