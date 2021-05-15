6075b9492eb1e52a35fec61d5eb94d8733ff0465
Sameer Ali <32299374+TheSameerAli@users.noreply.github.com>
2021-05-16 00:05:21 +0100

    fix(transaction-table): Orders trasaction by date (#41)

--- web/src/app/accounts/store/selector/account.selectors.ts
+++ web/src/app/accounts/store/selector/account.selectors.ts
@@ -1,6 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromAccount from '../reducer/account.reducer';
-
+import _, { map} from 'underscore';
+import * as moment from 'moment';
 
 export const selectAccountsState = createFeatureSelector<fromAccount.AccountState>(
   fromAccount.ACCOUNT_STATE_NAME,
@@ -23,7 +24,9 @@ export const accountsIsLoadingSelector = createSelector(
 export const currentlyViewingAccountTransactionsSelector = createSelector(
   selectAccountsState,
   (state: fromAccount.AccountState) => {
-    return state[0].currentlyViewingAccount.transactions;
+    let transactions = state[0].currentlyViewingAccount.transactions;
+    let sortedTransactions = arrayReverseObj(Object.assign([], _.groupBy(transactions, (tr) => moment(tr.transactionDate).format('MM/DD/YYYY'))));
+    return sortedTransactions;
   }
 )
 
@@ -33,3 +36,18 @@ export const currentlyViewingAccountSelector = createSelector(
     return state[0].currentlyViewingAccount.account;
   }
 )
+
+const arrayReverseObj = (obj) => {
+  let newArray = []
+
+  Object.keys(obj)
+    .sort()
+    .reverse()
+    .forEach(key => {
+      newArray.push( {
+      'date':key,
+      'transactions':obj[key]
+      })
+    })
+  return newArray
+}
