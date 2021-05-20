0afa8097bb8893e6698b25a2c362f8ea2e6f4c29
Sameer Ali <sam.eer.ali@hotmail.co.uk>
2021-05-20 14:35:21 +0100

    FIN-36 fix(login-validation): Fixes login page validation

--- web/src/app/authentication/_pages/login-page/login-page.component.ts
+++ web/src/app/authentication/_pages/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { TokenStorageService } from './../../_services/token-storage.service';
 import { AuthService } from './../../_services/auth.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -11,32 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
   public isLoading = false;
-  public loginForm: LoginFormViewModel = {
-    email: '',
-    password: ''
-  }
+  public loginForm: FormGroup;
   public isFailed = false;
   public errorMessage = '';
   constructor(private authService: AuthService,
     private tokenStorageService: TokenStorageService,
-    private router: Router) { }
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]]
+    })
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 
   login() {
-    this.isLoading = true;
-    this.isFailed = false;
-    this.authService.login(this.loginForm.email, this.loginForm.password).subscribe(data => {
-      this.isLoading = false;
-      this.tokenStorageService.saveToken(data.token);
-      this.authService.authEvent.emit();
-      this.router.navigate(['']);
-    }, (err) => {
-      this.isLoading = false;
-      this.isFailed = true;
-      this.errorMessage = err.error.message;
-    })
+    if (!this.loginForm.invalid) {
+      this.isLoading = true;
+      this.isFailed = false;
+      
+      this.authService.login(this.username.value, this.password.value).subscribe(data => {
+        this.isLoading = false;
+        this.tokenStorageService.saveToken(data.token);
+        this.authService.authEvent.emit();
+        this.router.navigate(['']);
+      }, (err) => {
+        this.isLoading = false;
+        this.isFailed = true;
+        this.errorMessage = err.error.message;
+      })
+    }
+
   }
 
 }
