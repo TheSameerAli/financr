5cddfe7dbfa720d6ca494cd88e58675a8f200281
Sameer Ali <sam.eer.ali@hotmail.co.uk>
2021-06-11 16:30:31 +0100

    wip

---
+++ api/Tests.Unit/ServiceLevelTests/Account/GetAccountPreferencesTests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Tests.Unit.ServiceLevelTests.Account.Base;
+using WebApi.Models.Database.Account;
+using WebApi.Models.Domain;
+using Xunit;
+
+namespace Tests.Unit.ServiceLevelTests.Account
+{
+    public class GetAccountPreferencesTests : AccountServiceTestBase
+    {
+        [Fact]
+        public async Task It_should_get_account_preferences_prvided_the_account_id()
+        {
+            #region Arrange
+            var account1 = await _helper.CreateAccount("Account 1", AccountType.Current, _testUser.Id);
+            var accountCategory1 = await _helper.CreateAccountCategory("testcategory", AccountCategoryType.Income, account1.Id);
+            var preferences1 = new AccountPreferences("GBP", account1.Id);
+            await _context.AccountPreferences.AddAsync(preferences1);
+            await _context.SaveChangesAsync();
+            #endregion
+
+
+            #region Act
+
+            var preferences = await _accountService.GetAccountPreferences(account1.Id);
+
+            #endregion
+
+            #region Assert
+
+            Assert.NotNull(preferences);
+            Assert.Equal("GBP", preferences.Currency);
+
+            #endregion
+        }
+    }
+}
