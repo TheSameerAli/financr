6a203729d6399419894415833e078070d7d0c7b6
Sameer Ali <sam.eer.ali@hotmail.co.uk>
2022-02-05 16:45:09 +0000

    Update version WIP

--- api/WebApi/Startup.cs
+++ api/WebApi/Startup.cs
@@ -24,8 +24,6 @@ namespace WebApi
         }
 
         private IConfiguration Configuration { get; }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCoreServices();
